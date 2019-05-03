4352fc5a55498c4b5fca0c68745e1a8b23815186
MarryMezentseva <mahaha20042015@gmail.com>
2019-05-03 17:34:04 +0300

    refactored

--- src/main/java/logger/TestLog.java
+++ src/main/java/logger/TestLog.java
@@ -3,9 +3,11 @@ package logger;
 public class TestLog {
     public static void main(String[] args) {
 
-        MyLog.getInstance().logInfo("some info");
-        MyLog.getInstance().logWarning("this is a warning");
-        MyLog.getInstance().logError(1547956);
+        MyLog myLog = MyLog.getInstance();
+
+        myLog.logInfo("some info");
+        myLog.logWarning("this is a warning");
+        myLog.logError(1547956);
     }
 }
 
